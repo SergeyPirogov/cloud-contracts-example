cf7c19f643df3328b1f10f76d822c5b42774f156
SergeyPirogov <semen4ik20@gmail.com>
2020-09-15 14:53:23 +0300

    add contracts for producer

---
+++ producer/src/test/java/com/example/producer/test/BaseTest.java
@@ -0,0 +1,26 @@
+package com.example.producer.test;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.verifier.messaging.boot.AutoConfigureMessageVerifier;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.context.WebApplicationContext;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@DirtiesContext
+@AutoConfigureMessageVerifier
+public class BaseTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @BeforeEach
+    void setup() {
+        RestAssuredMockMvc.webAppContextSetup(context);
+    }
+}
---
+++ producer/src/test/resources/contracts/shouldReturnEvenWhenRequestParamIsEven.groovy
@@ -0,0 +1,17 @@
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+    description "should return true when age is 31"
+    request{
+        method GET()
+        url("/age") {
+            queryParameters {
+                parameter("year", "29/07/1989")
+            }
+        }
+    }
+    response {
+        body("{ \"age\": 31 } ")
+        status 200
+    }
+}
