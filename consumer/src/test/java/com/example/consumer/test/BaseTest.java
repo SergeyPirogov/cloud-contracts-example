6c1984ff9bce287ce7591ab36ed2d61738027146
SergeyPirogov <semen4ik20@gmail.com>
2020-09-26 13:14:11 +0300

    comment cc contracts

--- consumer/src/test/java/com/example/consumer/test/BaseTest.java
+++ consumer/src/test/java/com/example/consumer/test/BaseTest.java
@@ -6,8 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
-import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
+//import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+//import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,9 +20,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 @AutoConfigureJsonTesters
-@AutoConfigureStubRunner(
-        stubsMode = StubRunnerProperties.StubsMode.LOCAL,
-        ids = "com.example.producer:producer:+:stubs:8086")
+//@AutoConfigureStubRunner(
+//        stubsMode = StubRunnerProperties.StubsMode.LOCAL,
+//        ids = "com.example.producer:producer:+:stubs:8086")
 public class BaseTest {
 
     @Autowired
