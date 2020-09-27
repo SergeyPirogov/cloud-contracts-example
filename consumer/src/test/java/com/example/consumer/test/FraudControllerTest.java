566b334ff8c955831a23066f1f2c4a9c6a61ed6b
SergeyPirogov <semen4ik20@gmail.com>
2020-09-27 19:21:56 +0300

    add fraud ok test

--- consumer/src/test/java/com/example/consumer/test/FraudControllerTest.java
+++ consumer/src/test/java/com/example/consumer/test/FraudControllerTest.java
@@ -10,10 +10,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class FraudControllerTest extends BaseTest{
 
     @Test
-    void testFraudController() throws Exception {
+    void testFraudControllerAmountFraud() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/credit?amount=99999&clientId=1234567890")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().string("{\"fraudCheckStatus\":\"FRAUD\",\"rejection.reason\":\"Amount too high\"}"));
+    }
+
+    @Test
+    void testFraudControllerAmountOk() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/credit?amount=9999&clientId=1234567890")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("{\"fraudCheckStatus\":\"OK\",\"acceptance.reason\":\"Amount OK\"}"));
     }
 }
